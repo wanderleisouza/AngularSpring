59610b22671919d6c3b6c2a22072ec536f9d06e4
Wanderlei Souza <wandi.darko@gmail.com>
2018-08-23 20:47:04 -0300

    First commit

---
+++ aula01/inference.js
@@ -0,0 +1,9 @@
+function showLog(message) {
+    console.log(message);
+}
+showLog('Hello World');
+showLog(23);
+var test = ['Hello', 'World'];
+var test2 = test;
+var isEnabled = true;
+//# sourceMappingURL=inference.js.map
---
+++ aula01/inference.ts
@@ -0,0 +1,10 @@
+function showLog(message: any) {
+    console.log(message);
+}
+
+showLog('Hello World');
+showLog(23);
+
+let test: Array<string> = ['Hello', 'World'];
+let test2: string[] = test; 
+let isEnabled: boolean = true;
